7067489929a26f8df7405893aa2edb05bf174af7
David Chau <davidchauprogrammer@gmail.com>
2016-02-20 10:15:24 -0800

    Adding necessary requires

--- Server.rb
+++ Server.rb
@@ -1,26 +1,26 @@
 require 'socket'
-​
+require_relative 'Response'
+
 class WebServer
   attr_reader :options
-​
+
   DEFAULT_PORT = 56789
+
   def initialize(options={})
     @options = options
   end
-​
+
   def start
     loop do
       puts "Listening for connections"
       client = server.accept
-​
       puts "Connection received"
-      RequestObj = Request.new(stream)
-      ResponseObj = Response.new()
-​
-      client.close
+      puts client.gets
+      requestObj = Request.new(stream).parse()
+      puts Response.new.to_s
     end
   end
-​
+
   def server
     @server ||= TCPServer.open(options.fetch(:port, DEFAULT_PORT))
   end
